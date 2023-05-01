9696e8b8753cf9887c744c04d5bb3ebeca817a17
gharbaoui <mohamed9gharbaoui@gmail.com>
2023-05-01 19:06:57 +0100

    make os that print string

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ### Build my operating system for fun
+
+![](./pics/halt_os.png)
+![](./pics/print_os.png)
--- src/main.s
+++ src/main.s
@@ -1,8 +1,28 @@
 org 0X7C00
 bits 16
 
+%define ENDL 0x0D, 0x0A
+
 _start:
 	jmp main
+
+
+print_string:
+	push ax
+	push si
+
+	mov bh, 0x00
+	mov ah, 0x0e
+.loop:
+	lodsb
+	or al, al
+	jz .done
+	int 0x10
+	jmp .loop
+.done:
+	pop si
+	pop ax
+	ret
 
 main:
 	mov ax, 0x0
@@ -12,6 +32,12 @@ main:
 
 	mov ss, ax
 	mov sp, 0X7C00
+	
+	mov si, msg
+	call print_string
+	hlt
+
+msg: db "intro to operating systems", ENDL, 0
 
 times 510-($-$$) db 0
 dw 0AA55h
